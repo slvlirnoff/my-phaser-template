312cc0819839f53a6348111bf01d0473a3312a6b
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-03-21 11:15:50 -0300

    Move game states importsto a module

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,16 @@
 import Phaser from 'Phaser';
 
-import Boot    from './app/states/Boot';
-import Preload from './app/states/Preload';
-import Menu    from './app/states/Menu';
-import Credits from './app/states/Credits';
-import Levels  from './app/states/Levels';
-import Game    from './app/states/Game';
+// Import all declared states from this module into an object.
+import * as states from './app/states';
 
 
 export function start () {
   var game = new Phaser.Game(480, 720, Phaser.AUTO);
 
-  // Adding game states to Phaser can be as simple as that.
-  [
-    [ 'Boot'   , Boot    ],
-    [ 'Preload', Preload ],
-    [ 'Menu'   , Menu    ],
-    [ 'Credits', Credits ],
-    [ 'Levels' , Levels  ],
-    [ 'Game'   , Game    ]
-  ].map(([ name, state ]) => game.state.add(name, state));
+  // Dynamically adding all required game states.
+  Object.keys(states)
+    .map((key) => [ key, states[key] ])
+    .forEach((args) => game.state.add(... args));
 
   game.state.start('Boot');
 
---
+++ src/scripts/app/states.js
@@ -0,0 +1,7 @@
+// Use this module to expose all states required by your game.
+export { default as Boot    } from './states/Boot';
+export { default as Preload } from './states/Preload';
+export { default as Menu    } from './states/Menu';
+export { default as Credits } from './states/Credits';
+export { default as Levels  } from './states/Levels';
+export { default as Game    } from './states/Game';
